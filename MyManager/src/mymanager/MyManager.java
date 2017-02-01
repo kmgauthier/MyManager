154eea7b16675a0d98aead9ccf45368c06b830ce
mfair55 <mfair@oakland.edu>
2017-02-01 13:48:49 -0500

    Create a System.out

--- MyManager/src/mymanager/MyManager.java
+++ MyManager/src/mymanager/MyManager.java
@@ -15,7 +15,9 @@ public class MyManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("This is a test");
+
     }
     
 }
