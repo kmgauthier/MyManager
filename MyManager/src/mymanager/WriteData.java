e919ec818939c497be31364ee3a9d902130ce586
Kristina Gauthier <kmgauthier@oakland.edu>
2017-02-13 16:24:18 -0500

    added to the write data

--- MyManager/src/mymanager/WriteData.java
+++ MyManager/src/mymanager/WriteData.java
@@ -11,17 +11,21 @@ import java.io.Serializable;
  */
 public class WriteData implements Serializable{
     
-    private int weight, age;
+    private int weight, age, startBalance, startSavings, savingsGoal;
     private Height height;
-    private String firstName, lastName;
+    private String firstName, lastName, date;
     private boolean hasData = false;
     
     
-    public WriteData(Height newHeight, int newWeight, int newAge, String newFirstName, String newLastName){
+    public WriteData(Height newHeight, int newWeight, int newAge, String newFirstName, String newLastName, 
+            int newStartBalance, int newStartSavings, int newSaveGoal){
         setWeight(newWeight);
         setHeight(newHeight);
         setAge(newAge);
         setName(newFirstName, newLastName);
+        setStartBalance(newStartBalance);
+        setStartSavings(newStartSavings);
+        setSavingsGoal(newSaveGoal);
         hasData = true;
     }
     
@@ -54,6 +58,33 @@ public class WriteData implements Serializable{
         lastName = newLastName;
     }
     
+    public void setStartBalance(int newStartBalance){
+        if(newStartBalance >= 0){
+            startBalance = newStartBalance;
+        }
+        else{
+            startBalance = 0;
+        }
+    }
+    
+    public void setStartSavings(int newStartSavings ){
+        if( newStartSavings >=0 ){
+            startSavings = newStartSavings;
+        }
+        else{
+            startSavings = 0;
+        }
+    }
+    
+    public void setSavingsGoal(int newSaveGoal){
+        if( newSaveGoal >0 ){
+            savingsGoal = newSaveGoal;
+        }
+        else{
+            //error
+        }
+    }
+    
     public int getWeight(){
         return weight;
     }
@@ -74,11 +105,22 @@ public class WriteData implements Serializable{
         return lastName;
     }
     
+    public int getStartSavings(){
+        return startSavings;
+    }
     
+    public int getStartBalance(){
+        return startBalance;
+    }
 
+    public int getSavingsGoal(){
+        return savingsGoal;
+    }
+    
     @Override
     public String toString(){
-        return "Name: "+firstName+" "+lastName+", Age: "+age+", Height: "+height+", Weight: "+weight;
+        return "Name: "+firstName+" "+lastName+", Age: "+age+", Height: "+height+", Weight: "+weight + 
+                ", Starting Balance: $" + startBalance + ", Starting SAvings: $" + startSavings + ", Goal Savings: $" + savingsGoal;
     }
     
     
